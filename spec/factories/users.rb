a352f48233e1157c41bacb78da7f71cad1fbb857
kyo-su <83508597+kyo-su@users.noreply.github.com>
2021-07-12 23:53:52 +0900

    FactoryBotのgimeiのデータを修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,16 @@
 FactoryBot.define do
   factory :user do
+    gimei = Gimei.new
+
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
     # password              {Faker::Internet.password(min_length: 6)}
     password              { Faker::Lorem.characters(number: 8, min_alpha: 4, min_numeric: 1) }
     password_confirmation { password }
-    family_name_kanji     { Gimei.last.kanji }
-    first_name_kanji      { Gimei.first.kanji }
-    family_name_kana      { Gimei.last.katakana }
-    first_name_kana       { Gimei.first.katakana }
+    family_name_kanji     { gimei.name.last.kanji }
+    first_name_kanji      { gimei.name.first.kanji }
+    family_name_kana      { gimei.name.last.katakana }
+    first_name_kana       { gimei.name.first.katakana }
     birthday              { '2000-01-01' }
   end
 end
