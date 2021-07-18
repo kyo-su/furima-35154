f594fb8530d73d3423ec8d681e18f547b18166c1
kyo-su <83508597+kyo-su@users.noreply.github.com>
2021-07-18 23:45:51 +0900

    ActiveHashの設定とviewで表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,26 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  # ActiveHashのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :charge_burden
+  belongs_to :prefecture
+  belongs_to :day
+
+  # 空の投稿を保存できないようにする
+  with_options presence: true do
+    validates :title
+    validates :description
+  end
+
+  # 選択肢「---」の時は保存できないようにする
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :status_id
+    validates :charge_burden_id
+    validates :prefecture_id
+    validates :day_id
+  end
 end
