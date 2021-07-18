7498d8efeaf2ee721d6c2fec55129f0a7f691d2e
kyo-su <83508597+kyo-su@users.noreply.github.com>
2021-07-19 01:55:18 +0900

    ストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,5 +6,10 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :title, :description, :category_id, :status_id, :charge_burden_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
+  end
 
 end
