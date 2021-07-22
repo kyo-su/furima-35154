5d0e003919758e9068b34bfb4477b37976210809
kyo-su <83508597+kyo-su@users.noreply.github.com>
2021-07-22 23:48:14 +0900

    itemモデルの単体テストを実施

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    title { Faker::Lorem.sentence }
+    description { Faker::Lorem.sentence }
+    category_id { rand(2..11) }
+    status_id { rand(2..7) }
+    charge_burden_id { rand(2..3) }
+    prefecture_id { rand(2..48) }
+    day_id { rand(2..4) }
+    price { rand(300..9999999) }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,89 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do 
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '出品した商品の登録' do
+    context '商品が登録できるとき' do
+      it '商品画像、商品名、商品の説明、カテゴリー、商品の状態、配送料の負担、発送元の地域、発送までの日数、価格があれば保存できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品が登録できないとき' do
+      it '商品画像がないと保存できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名がないと保存できない' do
+        @item.title = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Title can't be blank")
+      end
+      it '商品の説明がないと保存できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'カテゴリーがないと保存できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品の状態がないと保存できない' do
+        @item.status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+      it '配送料の負担がないと保存できない' do
+        @item.charge_burden_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Charge burden can't be blank")
+      end
+      it '発送元の地域がないと保存できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '発送までの日数がないと保存できない' do
+        @item.day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Day can't be blank")
+      end
+      it '価格がないと保存できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'ユーザーが紐づいていないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+      it '価格は半角数字でないと保存できない（全角の場合）' do
+        @item.price = 'あああ'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it '価格は半角数字でないと保存できない（半角英字の場合）' do
+        @item.price = 'aaa'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it '価格の入力値は300~9,999,999以内でないと保存できない（300以下の場合）' do
+        @item.price = '100'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than 300")
+      end
+      it '価格の入力値は300~9,999,999以内でないと保存できない（9,999,999以上の場合）' do
+        @item.price = '100000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than 9999999")
+      end
+    end
+  end
 end
