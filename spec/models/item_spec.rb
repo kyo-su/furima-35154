7c75d5cd332286b2699a9ccf9dcda3b5c4fed2c7
kyo-su <83508597+kyo-su@users.noreply.github.com>
2021-07-23 14:58:54 +0900

    修正内容を反映

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -58,6 +58,37 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
+
+      it 'カテゴリーに1が選択された場合出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it '商品の状態に1が選択された場合出品できない' do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+
+      it '配送料の負担に1が選択された場合出品できない' do
+        @item.charge_burden_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Charge burden can't be blank")
+      end
+
+      it '発送元の地域に1が選択された場合出品できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it '発送までの日数に1が選択された場合出品できない' do
+        @item.day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Day can't be blank")
+      end
+
       it 'ユーザーが紐づいていないと保存できない' do
         @item.user = nil
         @item.valid?
@@ -73,15 +104,22 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
-      it '価格の入力値は300~9,999,999以内でないと保存できない（300以下の場合）' do
-        @item.price = '100'
+
+      it '価格は半角数字でないと保存できない（半角英数混合の場合）' do
+        @item.price = 'aaa111'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be greater than 300')
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+
+      it '価格の入力値は300~9,999,999以内でないと保存できない（300以下の場合）' do
+        @item.price = 100
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than 299')
       end
       it '価格の入力値は300~9,999,999以内でないと保存できない（9,999,999以上の場合）' do
-        @item.price = '100000000'
+        @item.price = 100_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be less than 9999999')
+        expect(@item.errors.full_messages).to include('Price must be less than 10000000')
       end
     end
   end
