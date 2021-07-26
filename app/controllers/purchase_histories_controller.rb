84e9cdd7638cdd04f598add4ad2b80c1217ddff4
kyo-su <83508597+kyo-su@users.noreply.github.com>
2021-07-27 02:15:00 +0900

    コントローラーを作成、viewを配置

---
+++ app/controllers/purchase_histories_controller.rb
@@ -0,0 +1,5 @@
+class PurchaseHistoriesController < ApplicationController
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, only: [:new, :create]
+  resources :purchase_histories, only: :index
 end
