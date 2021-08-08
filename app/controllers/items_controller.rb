f7b218bce359eaf14ab1186155df66f1901389bb
kyo-su <83508597+kyo-su@users.noreply.github.com>
2021-08-08 17:19:07 +0900

    rubocopを実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,7 +36,6 @@ class ItemsController < ApplicationController
     end
   end
 
-
   private
 
   def item_params
@@ -47,8 +46,6 @@ class ItemsController < ApplicationController
   def move_to_index
     item = Item.find(params[:id])
 
-    unless user_signed_in? && current_user.id == item.user.id
-        redirect_to root_path
-    end
+    redirect_to root_path unless user_signed_in? && current_user.id == item.user.id
   end
 end
